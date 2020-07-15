ba77e538dc4849f1088f560b95b3dea952382636
Chuck <fanck0605@qq.com>
2020-07-15 14:31:41 +0800

    initial commit
    Signed-off-by: Chuck <fanck0605@qq.com>

---
+++ README.md
@@ -0,0 +1,13 @@
+# 基于原生 OpenWrt 的 NanoPi R2s 固件
+
+## 温情提醒
+
+- WAN 和 LAN 默认是**互换**的，请注意网线接法。
+
+- 刷机请**不要**保留配置，任何因保留配置导致的问题不给予解决！
+
+## 说明
+
+* ipv4: 192.168.2.1
+* username: root
+* password: password
---
+++ build.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -eu
+
+rm -rf openwrt
+git clone https://github.com/coolsnowwolf/lede.git openwrt
+
+# customize patches
+pushd openwrt
+git am -3 ../patches/*.patch
+popd
+
+# addition packages
+pushd openwrt/package
+# luci-theme-argon
+rm -rf lean/luci-theme-argon
+git clone --depth 1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git lean/luci-theme-argon
+# luci-app-filebrowser
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-filebrowser lean/luci-app-filebrowser
+svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/filebrowser lean/filebrowser
+# luci-app-oled
+git clone --depth 1 https://github.com/NateLol/luci-app-oled.git lean/luci-app-oled
+popd
+
+# initialize feeds
+p_list=$(ls -l patches | grep ^d | awk '{print $NF}')
+pushd openwrt
+# clone feeds
+./scripts/feeds update -a
+# patching
+pushd feeds
+for p in $p_list ; do
+  [ -d $p ] && {
+    pushd $p
+    git am -3 ../../../patches/$p/*.patch
+    popd
+  }
+done
+popd
+popd
+
+#install packages
+pushd openwrt
+./scripts/feeds install -a
+popd
+
+# customize configs
+pushd openwrt
+cat ../config.seed > .config
+make defconfig
+popd
+
+# build openwrt
+pushd openwrt
+make download -j8
+make -j$(($(nproc) + 1)) || make -j1 V=s
+popd
