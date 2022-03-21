c6d0180de2f2efca8d27375084d7956f6234d538
Chuck Fan <fanck0605@qq.com>
2022-03-21 18:09:58 +0800

    将各部分操作分离成函数

--- build.sh
+++ build.sh
@@ -92,125 +92,137 @@ download_clash_files() {
 	return 0
 }
 
-# clone openwrt
-cd "$PROJ_DIR"
-echo "开始初始化 OpenWrt 源码"
-echo "当前目录: ""$(pwd)"
-if [ -d "./openwrt" ] && [ -d "./openwrt/.git" ]; then
-	echo "OpenWrt 源码已存在"
-	pushd ./openwrt
-	echo "开始清理 OpenWrt 源码"
-	find ./!(.git|feeds) -name .git -exec rm -rf {} +
-	git clean -dfx
-	echo "开始更新 OpenWrt 源码"
-	git fetch origin "$VERSION"
-	git reset --hard "$VERSION"
-	popd
-else
-	echo "OpenWrt 源码不存在"
-	echo "开始克隆 OpenWrt 源码"
-	git clone -b "$VERSION" https://github.com/openwrt/openwrt.git openwrt
-fi
-echo "OpenWrt 源码初始化完毕"
-
-# patch openwrt
-cd "$PROJ_DIR/openwrt"
-echo "开始修补 OpenWrt 源码"
-echo "当前目录: ""$(pwd)"
-cp -lr "$PROJ_DIR/files/openwrt"/* ./
-# 因为使用了软链接, 尽量使用相对目录
-apply_patches ../patches/openwrt
-echo "OpenWrt 源码修补完毕"
-
-# clone feeds
-cd "$PROJ_DIR/openwrt"
-echo "Initializing OpenWrt feeds..."
-echo "Current directory: ""$(pwd)"
-awk '/^src-git/ { print $2 }' feeds.conf.default | while IFS= read -r feed; do
-	if [ -d "./feeds/$feed" ]; then
-		pushd "./feeds/$feed"
-		find ./!(.git) -name .git -exec rm -rf {} +
-		git reset --hard
+prepare() {
+	# clone openwrt
+	cd "$PROJ_DIR"
+	echo "开始初始化 OpenWrt 源码"
+	echo "当前目录: ""$(pwd)"
+	if [ -d "./openwrt" ] && [ -d "./openwrt/.git" ]; then
+		echo "OpenWrt 源码已存在"
+		pushd ./openwrt
+		echo "开始清理 OpenWrt 源码"
+		find ./!(.git|feeds) -name .git -exec rm -rf {} +
 		git clean -dfx
+		echo "开始更新 OpenWrt 源码"
+		git fetch origin "$VERSION"
+		git reset --hard "$VERSION"
 		popd
+	else
+		echo "OpenWrt 源码不存在"
+		echo "开始克隆 OpenWrt 源码"
+		git clone -b "$VERSION" https://github.com/openwrt/openwrt.git openwrt
 	fi
-done
-./scripts/feeds update -a
+	echo "OpenWrt 源码初始化完毕"
 
-# patch feeds
-echo "Patching OpenWrt feeds..."
-echo "Current directory: ""$(pwd)"
-cd "$PROJ_DIR/openwrt"
-awk '/^src-git/ { print $2 }' feeds.conf.default | while IFS= read -r feed; do
-	if [ -d "$PROJ_DIR/files/$feed" ]; then
-		cd "$PROJ_DIR/openwrt/feeds/$feed"
-		cp -lr "$PROJ_DIR/files/$feed"/* ./
-	fi
-	if [ -d "$PROJ_DIR/patches/$feed" ]; then
-		cd "$PROJ_DIR/openwrt/feeds/$feed"
-		# 因为使用了软链接, 尽量使用相对目录
-		apply_patches ../../../patches/"$feed"
-	fi
-done
+	# patch openwrt
+	cd "$PROJ_DIR/openwrt"
+	echo "开始修补 OpenWrt 源码"
+	echo "当前目录: ""$(pwd)"
+	cp -lr "$PROJ_DIR/files/openwrt"/* ./
+	# 因为使用了软链接, 尽量使用相对目录
+	apply_patches ../patches/openwrt
+	echo "OpenWrt 源码修补完毕"
 
-# addition packages
-cd "$PROJ_DIR/openwrt"
-# luci-app-openclash
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/custom/luci-app-openclash
-download_clash_files package/custom/luci-app-openclash/root armv8
-# luci-app-arpbind
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-arpbind feeds/luci/applications/luci-app-arpbind
-# luci-app-xlnetacc
-svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-xlnetacc feeds/luci/applications/luci-app-xlnetacc
-# luci-app-oled
-git clone --depth 1 https://github.com/NateLol/luci-app-oled.git package/custom/luci-app-oled
-# luci-app-unblockmusic
-svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/luci-app-unblockneteasemusic package/custom/luci-app-unblockneteasemusic
-svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/UnblockNeteaseMusic package/custom/UnblockNeteaseMusic
-# luci-app-autoreboot
-svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-autoreboot feeds/luci/applications/luci-app-autoreboot
-# luci-app-vsftpd
-svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-vsftpd feeds/luci/applications/luci-app-vsftpd
-rm -rf ./feeds/packages/net/vsftpd
-svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/vsftpd feeds/packages/net/vsftpd
-# luci-app-netdata
-svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-netdata feeds/luci/applications/luci-app-netdata
-# ddns-scripts
-svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/ddns-scripts_aliyun feeds/packages/net/ddns-scripts_aliyun
-svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/ddns-scripts_dnspod feeds/packages/net/ddns-scripts_dnspod
-# luci-theme-argon
-git clone -b master --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
-# luci-app-uugamebooster
-svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-uugamebooster feeds/luci/applications/luci-app-uugamebooster
-svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/uugamebooster feeds/packages/net/uugamebooster
+	# clone feeds
+	cd "$PROJ_DIR/openwrt"
+	echo "Initializing OpenWrt feeds..."
+	echo "Current directory: ""$(pwd)"
+	awk '/^src-git/ { print $2 }' feeds.conf.default | while IFS= read -r feed; do
+		if [ -d "./feeds/$feed" ]; then
+			pushd "./feeds/$feed"
+			find ./!(.git) -name .git -exec rm -rf {} +
+			git reset --hard
+			git clean -dfx
+			popd
+		fi
+	done
+	./scripts/feeds update -a
 
-# install packages
-cd "$PROJ_DIR/openwrt"
-# 在添加自定义软件包后必须再次 update
-./scripts/feeds update -a
-./scripts/feeds install -a
+	# patch feeds
+	echo "Patching OpenWrt feeds..."
+	echo "Current directory: ""$(pwd)"
+	cd "$PROJ_DIR/openwrt"
+	awk '/^src-git/ { print $2 }' feeds.conf.default | while IFS= read -r feed; do
+		if [ -d "$PROJ_DIR/files/$feed" ]; then
+			cd "$PROJ_DIR/openwrt/feeds/$feed"
+			cp -lr "$PROJ_DIR/files/$feed"/* ./
+		fi
+		if [ -d "$PROJ_DIR/patches/$feed" ]; then
+			cd "$PROJ_DIR/openwrt/feeds/$feed"
+			# 因为使用了软链接, 尽量使用相对目录
+			apply_patches ../../../patches/"$feed"
+		fi
+	done
 
-# zh_cn to zh_Hans
-cd "$PROJ_DIR/openwrt/package"
-"$PROJ_DIR/scripts/convert_translation.sh"
+	# addition packages
+	cd "$PROJ_DIR/openwrt"
+	# luci-app-openclash
+	svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/custom/luci-app-openclash
+	download_clash_files package/custom/luci-app-openclash/root armv8
+	# luci-app-arpbind
+	svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-arpbind feeds/luci/applications/luci-app-arpbind
+	# luci-app-xlnetacc
+	svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-xlnetacc feeds/luci/applications/luci-app-xlnetacc
+	# luci-app-oled
+	git clone --depth 1 https://github.com/NateLol/luci-app-oled.git package/custom/luci-app-oled
+	# luci-app-unblockmusic
+	svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/luci-app-unblockneteasemusic package/custom/luci-app-unblockneteasemusic
+	svn co https://github.com/cnsilvan/luci-app-unblockneteasemusic/trunk/UnblockNeteaseMusic package/custom/UnblockNeteaseMusic
+	# luci-app-autoreboot
+	svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-autoreboot feeds/luci/applications/luci-app-autoreboot
+	# luci-app-vsftpd
+	svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-vsftpd feeds/luci/applications/luci-app-vsftpd
+	rm -rf ./feeds/packages/net/vsftpd
+	svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/vsftpd feeds/packages/net/vsftpd
+	# luci-app-netdata
+	svn co https://github.com/coolsnowwolf/luci/trunk/applications/luci-app-netdata feeds/luci/applications/luci-app-netdata
+	# ddns-scripts
+	svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/ddns-scripts_aliyun feeds/packages/net/ddns-scripts_aliyun
+	svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/ddns-scripts_dnspod feeds/packages/net/ddns-scripts_dnspod
+	# luci-theme-argon
+	git clone -b master --depth 1 https://github.com/jerrykuku/luci-theme-argon.git package/custom/luci-theme-argon
+	# luci-app-uugamebooster
+	svn co https://github.com/immortalwrt/luci/branches/openwrt-21.02/applications/luci-app-uugamebooster feeds/luci/applications/luci-app-uugamebooster
+	svn co https://github.com/immortalwrt/packages/branches/openwrt-21.02/net/uugamebooster feeds/packages/net/uugamebooster
 
-# create acl files
-cd "$PROJ_DIR/openwrt"
-"$PROJ_DIR/scripts/create_acl_for_luci.sh" -a
-"$PROJ_DIR/scripts/create_acl_for_luci.sh" -c
+	# install packages
+	cd "$PROJ_DIR/openwrt"
+	# 在添加自定义软件包后必须再次 update
+	./scripts/feeds update -a
+	./scripts/feeds install -a
 
-# customize configs
-cd "$PROJ_DIR/openwrt"
-cat "$PROJ_DIR/config.seed" >.config
-make defconfig
+	# zh_cn to zh_Hans
+	cd "$PROJ_DIR/openwrt/package"
+	"$PROJ_DIR/scripts/convert_translation.sh"
+
+	# create acl files
+	cd "$PROJ_DIR/openwrt"
+	"$PROJ_DIR/scripts/create_acl_for_luci.sh" -a
+	"$PROJ_DIR/scripts/create_acl_for_luci.sh" -c
+
+	# customize configs
+	cd "$PROJ_DIR/openwrt"
+	cat "$PROJ_DIR/config.seed" >.config
+	make defconfig
+
+	return 0
+}
+
+# build openwrt
+build() {
+	cd "$PROJ_DIR/openwrt"
+
+	make download -j16
+	make -j$(($(nproc) + 1)) || make -j1 V=s
+
+	mkdir -p "$PROJ_DIR"/artifact
+	cp -r ./bin/targets/*/*/* "$PROJ_DIR"/artifact/
+
+	return 0
+}
+
+prepare
 
 $MAINTAIN && exit 0
 
-# build openwrt
-cd "$PROJ_DIR/openwrt"
-make download -j8
-make -j$(($(nproc) + 1)) || make -j1 V=s
-
-# copy output files
-cd "$PROJ_DIR"
-cp -rf openwrt/bin/targets/*/* artifact
+build
