a3e7d14eaac502b1b91bfca22ea80fb9561b8b30
Chuck Fan <fanck0605@qq.com>
2022-07-09 13:39:20 +0800

    feat: 添加 China List 自动更新脚本

---
+++ packages/files/net/smartdns/files/update_chinalist.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+DNSMASQ_CHINA_LIST=https://github.com/felixonmars/dnsmasq-china-list/raw/master
+
+curl -sL "$DNSMASQ_CHINA_LIST/accelerated-domains.china.conf" | sed -n 's|^server=/\(.*\?\)/.*$|nameserver /\1/china|p' >/etc/smartdns/domains.china.conf
+curl -sL "$DNSMASQ_CHINA_LIST/google.china.conf" | sed -n 's|^server=/\(.*\?\)/.*$|nameserver /\1/china|p' >/etc/smartdns/google.china.conf
+curl -sL "$DNSMASQ_CHINA_LIST/apple.china.conf" | sed -n 's|^server=/\(.*\?\)/.*$|nameserver /\1/china|p' >/etc/smartdns/apple.china.conf
+
+/etc/init.d/smartdns reload
